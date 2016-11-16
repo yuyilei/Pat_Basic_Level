f97ae0e90756527331ad01c46c4b5dff0cef6847
yuyilei <yyl1352091742@gmail.com>
2016-11-16 21:46:11 +0800

    pat

--- l1-005.c
+++ l1-005.c
@@ -1,12 +1,12 @@
 /*考试座位号*/
-/*如何保证他们的信息是不一样的?*/
+/*如何保证他们的信息是不一样的?不用保证，自动输入*/
 
 #include<stdio.h>
-   struct  student {                                                           
-            char number[14];                                                         
+   struct  Student {                                                                                                  
+            long number; /*学号*/
             int  seat1 ;                                                             
             int  seat2 ;                                                             
-      };    
+   };
 
 int main(){
  
@@ -16,45 +16,31 @@ int main(){
     int m ;  /*要获取信息的人数*/
     int  k = 0 ; /*用于例遍数组*/
     int a[100] ;  /*用于存储考生的座位号*/
+    struct Student students[1000];
 
     scanf("%d",&n);   /*考生人数*/
     
-    for (i =0 ; i < n ; i++){
-    
-        struct student i ;
-    }   /*建立结构体*/
-
-    for ( i=0 ; i< n; i++){
-         for ( j=0; j <14 ;j++ ){
-            scanf("%s",&i.number[j]);
-         }
-
-         scanf("%d",&i.seat1);
-         scanf("%d",&i.seat2);
-        
-    }    /*得到学号等信息*/
+    for (i = 0 ; i < n ; i++){
+        struct Student student;
+        scanf("%ld", &student.number);
+        scanf("%d", &student.seat1);
+        scanf("%d", &student.seat2);
+        students[i] = student;
+    }
 
     scanf("%d",&m); /*要获取m个人的信息*/
     
     for( i = 0 ; i < m ; i++){
-     
-        scanf("%d", a[i]) ;
-    }     /*获取座位号*/
+        scanf("%d", &a[i]);
+    }
 
-
-    for (i =0 ; i<n ; i++ ) {
-      
-        for (  k= 0 ; k < m ; k++){
-        
-            if ( a[i] == k.seat2){
-            
-                printf("%s",k.number);
-                printf("%d",k.seat1);
-
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            if(students[j].seat1 == a[i]) {
+                printf("%ld %d\n", students[j].number, students[j].seat2);
             }
         }
     }
 
-   return  0; 
-
+    return  0; 
 }
