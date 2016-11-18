685057af78da2d2f13812d2cd351ca02a9355fac
yuyilei <yyl1352091742@gmail.com>
2016-11-18 23:22:11 +0800

    pat

--- 1002-2.c
+++ 1002-2.c
@@ -2,16 +2,15 @@
 
 int main(){
     char input[100] ;
-    long sum = 0 ;
-    int i =0 ;
-    int j = 0; 
+    long sum =0  ;
+    int i =0  ;
+    int j =0  ; 
     char a[100]  ;
-    
+     
    scanf("%c",&input);
 
-   for ( i = 0; input[i] ; i ++){
-   
-       sum += a[i] ;
+   for ( i = 0; input != '\n' ; i++){
+         sum += (int)(input[i] - 48 ); 
    }
 
    for (  j= 0; sum > 0 ; j ++){
@@ -19,9 +18,9 @@ int main(){
        a[j] = sum % 10 ;
        sum /= 10 ; 
    }       
-   while ( a [j] != NULL ){
+   while ( a[j] ){
           if ( j != 0) {
-                  switch ( a [j--] ){
+                  switch ( a[j--] ){
                     
                         case 0 : printf("ling ");  break ;
                         case 1 : printf("yi ");    break ;
@@ -38,18 +37,18 @@ int main(){
     
         }
           else {
-          switch ( a [j] ){                                           
+          switch ( a[j] ){                                           
                                                                                           
-                                  case 0 : printf("ling");  break ;                      
+                                   case 0 : printf("ling");  break ;                      
                                    case 1 : printf("yi");    break ;                      
                                    case 2 : printf("er");    break ;                      
                                    case 3 : printf("san");   break ;                      
                                    case 4 : printf("si");    break ;                      
                                    case 5 : printf("wu");    break ;                      
                                    case 6 : printf("liu");   break ;                      
-                                    case 7 : printf("qi");    break ;                      
-                                    case 8 : printf("ba");    break ;                      
-                                    case 9 : printf("jiu");   break ;
+                                   case 7 : printf("qi");    break ;                      
+                                   case 8 : printf("ba");    break ;                      
+                                   case 9 : printf("jiu");   break ;
           }
           
           }
