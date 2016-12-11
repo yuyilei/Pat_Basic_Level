3733a4c277b18fea911005627eec7c631dc6f50b
yuyilei <yyl1352091742@gmail.com>
2016-12-11 23:16:44 +0800

    pat

--- 1007.c
+++ 1007.c
@@ -1,14 +1,15 @@
 /*素数对猜想*/
 
 #include<stdio.h>
+
 int tell(long number){     //判断是否是素数
     int i ;
     int count = 0 ;
-    for ( i = 1 ; i <= number && count <= 3 ; i++ ){ 
+    for ( i = 3 ; (i <= number) && (count <= 1); i=i+2 ){ 
         if ( number % i == 0)
             count++ ;
     }
-    if ( count == 2)
+    if ( count == 1)
         return 1 ;
     else 
         return 0 ;
@@ -16,21 +17,38 @@ int tell(long number){     //判断是否是素数
 
 int main(){
 
+    long i = 0;
     long n  ;
     long prime = 3  ;
-    int  count = 0  ;
+    long count = 0  ;
  
     scanf("%ld",&n) ;
 
+    
     if ( n <= 3 )
-        printf("0") ;
+        printf("%d", 0) ;
 
     else {
-            for ( ; prime <=n ; prime = prime + 2 ) { 
-          if ( tell(prime) == 1 && tell(prime + 2) == 1 )
-                count++ ;
-      }
-            printf("%d",count) ;
-  }
+        for (prime=3; prime <=n-2 ; prime = prime + 2 ) { 
+          if ( tell(prime) == 1) {
+              if ( tell(prime+2) == 1 ) {
+                  count++;
+              }
+              else {
+                  prime += 2;
+              }
+          }
+        }
+        printf("%ld",count) ;
+    }
+    
+
+    for(i = 0; i< n; i++) {
+        if (tell(i) == 1) {
+            count++;
+        }
+    }
+    printf("%ld", count);
+
     return 0 ;
 }
