a2892984fa1383259b27b9ed9b0faec119caa60c
yuyilei <yyl1352091742@gmail.com>
2017-03-07 12:16:09 +0800

    14分啊QAQ

--- 1068.c
+++ 1068.c
@@ -2,54 +2,58 @@
 #include<math.h>
 #include<stdlib.h>
 
-int sub( long long a , long long b ,long long tol ) {
-
-    if ( ( a -b ) > tol  || ( b - a) > tol) 
-        return 1 ;
-    else 
-        return 0 ;
-        
-}
 
 int tell( long long c[][10000] , long long tol , int a , int b ){
     
     int i = a - 1 ; 
     int j = b - 1 ;
+    int flag = 0 ;
+    
+    if (c[a][b] - c[a-1][b-1] >0 )
+        flag = 1 ;
+    else 
+        flag = -1 ;
 
         for ( ; i <= a + 1 ; i++ ) {
         
-            if ( abs(c[a][b]-c[i][j]) > tol  ) {
+            if ( flag*(c[a][b] - c[i][j] ) <= tol  ) {
             
-                return 1 ; 
+                return 0 ; 
             }
         }
+
+        i = a + 1 ;
                 
         for ( ; j <= b + 1  ; j++ ) {
         
-            if ( sub(c[a][b],c[i][j],tol) == 0){
+            if ( flag*(c[a][b] - c[i][j] ) <=tol ) {
             
-                return 2 ;
+                return 0 ;
             }
         } 
 
+        j = b + 1 ;
+
         for ( ; i >= a - 1 ; i-- ) {
         
-            if ( sub(c[a][b],c[i][j],tol) == 0){
+            if ( flag*(c[a][b] - c[i][j] ) <= tol ){
             
-                return 3 ;
+                return 0 ;
             }
         }
 
+
+        i = a - 1 ;
 
         for ( ; j > b - 1 ; j-- ) {
         
-            if ( sub(c[a][b],c[i][j],tol) == 0) {
+            if ( flag*(c[a][b] - c[i][j] ) <= tol ) {
             
-                return 4;
+                return 0 ;
             }
         }
     
-        return 5 ;
+        return 1 ;
     
 
 } 
@@ -80,44 +84,27 @@ int main () {
     int flag1 = 0 ;
     int flag2 = 0 ;
 
-/*    for ( i = 1 ; i < m -1 ;  ){
+    for ( i = 1 ; i < m -1 ; i++ ){
     
-        for ( j = 1 ; j < n -1 ; ) {
+        for ( j = 1 ; j < n -1 ; j++ ) {
         
-            if ( tell(a,tol,i,j) == 0 ) {
-            
-                i += 2 ;
-                j += 1 ;
-                printf("!") ;
-            }
+            int temp = tell(a,tol,i,j) ;
 
-            else if ( flag1 == 0){
-            
-                flag1 = 1 ;
-                j ++ ;
-                x = i + 1 ;
-                y = j + 1 ;
-                res = a[i][j] ;
-                printf("?") ;
-            }
-
-            else if ( flag1 == 1) {
+            if ( flag1 == 1 && temp == 1) {
             
                 flag2 = 1 ;
                 break ;
             }
-        }
+            
+            if ( flag1 == 0 && temp == 1 ){
+            
+                flag1 = 1 ;
+                x = j + 1 ;
+                y = i + 1 ;
+                res = a[i][j] ;
+          }
 
-        if ( flag1 == 0 ){
-        
-            i++ ;
-        }
-
-        if ( flag1 == 1 && flag2 == 0 ){
-        
-            i++ ;
-        }
-
+       }
         if ( flag2 == 1)
             break ;
     }
@@ -127,9 +114,11 @@ int main () {
     else if (flag2 == 1)
         printf("Not Unique\n") ;
     else 
-        printf("(%d,%d): %lld",x,y,res) ;
-*/
-    printf("%d",tell(a,tol,1,1)) ;
+        printf("(%d, %d): %lld",x,y,res) ;
+
+//    printf("%d",tell(a,tol,2,4)) ;
+
+
    // printf("%d",sub(a[0][0],a[1][1],tol)) ; 
     return 0 ;
 
