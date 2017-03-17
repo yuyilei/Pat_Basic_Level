7791268912c3bc0b8a03d2969fa79c1ceaa78856
yuyilei <yyl1352091742@gmail.com>
2017-03-18 00:46:03 +0800

    死掉比活着舒服,好想死掉

---
+++ 1067-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string password, temp;
+    int n, cnt = 0;
+    cin >> password >> n;
+   // getchar(); 
+    while(1) {
+            getline(cin, temp);
+            if (temp != "#") {
+                        cnt++;
+                    } else {
+                                break;
+                            }
+            if (cnt <= n && temp == password) {
+                        cout << "Welcome in";
+                        break;
+                    } else if (cnt <= n && temp != password) {
+                                cout << "Wrong password: " << temp << endl;
+                                if (cnt == n) {
+                                                cout << "Account locked";
+                                                break;
+                                            }
+                            }
+        }
+    return 0;
+}
